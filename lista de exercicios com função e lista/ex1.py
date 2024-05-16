21a741b53515359b82e5402a81160a94ae8a335a
Beatriz Novais <79538036+triz14@users.noreply.github.com>
2024-05-15 22:42:29 -0300

    exercicios adicionados

--- lista de exercicios com função e lista/ex1.py
+++ lista de exercicios com função e lista/ex1.py
@@ -10,3 +10,25 @@ def soma(lista):
 
 a = soma(lista)
 print(a)
+
+
+def somar(lista_da_funcao):
+    soma = 0
+    for elem in lista_da_funcao:
+        soma += elem
+    return soma
+
+lista = [2,2,2,2,2]
+print(somar(lista))
+
+def maior_num(lista_da_func):
+    local_maior = 0
+    maior = lista[local_maior]
+    for i in range(len(lista_da_func)):
+        if lista[i] > maior:
+            maior = lista[i]
+            local_maior = i
+    return local_maior
+
+lista = [1,2,3,4,5,6,7,8,9,10]
+print(maior_num(lista))
